3396c866437ea99efa5dc35e205fa2b3d030400c
Pritam Barman <49247449+pritambarman121@users.noreply.github.com>
2022-02-18 12:58:19 +0530

    high_res

--- hIghest_resolution.py
+++ hIghest_resolution.py
@@ -3,5 +3,6 @@ from pytube import YouTube
 link = input("Enter link of your video :")
 yt = YouTube(link)
 stream =yt.streams.get_highest_resolution()
+#Choose Download Location
 stream.download("D:\\Project")
 print("\Downloaded Successfully".upper())
